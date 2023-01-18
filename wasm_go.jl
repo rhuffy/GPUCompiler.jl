0618d1b0a49c660d5db89022c4c919c151d00cb6
Raymond Huffman <rhuffman@mit.edu>
2023-01-18 05:22:02 -0500

    broken example

--- wasm_go.jl
+++ wasm_go.jl
@@ -8,20 +8,22 @@ import JSON
   end
 end
 
-struct FooStruct
-  bar::Int
-  baz::Int
+# struct FooStruct
+#   value::Float32
+# end
+
+# function square(x)
+#   x^2
+# end
+
+# function foo(x)
+#   return JSON.json(FooStruct(x, x))
+# end
+
+function foo(a)
+  A = [a]
+  return A[1]
 end
-
-function square(x)
-  x^2
-end
-
-function foo(x)
-  return JSON.json(FooStruct(x, x))
-end
-
-
 
 open("output.wasm", "w") do io
   # write(io, wasmfunction(square, Tuple{Int}))
